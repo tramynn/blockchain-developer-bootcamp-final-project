dbfa19deb622891f9a709fd2e90524badb390883
Tramy Nguyen <tramynn2@gmail.com>
2022-01-13 00:37:26 -0600

    Updated config

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+const path = require("path");
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
