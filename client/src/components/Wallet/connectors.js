0e466e027d9788d8bf89b90e3ada2e11a7057247
Tramy Nguyen <tramynn2@gmail.com>
2022-01-14 17:54:32 -0600

    Updated supported chains to kovan and local only;

--- client/src/components/Wallet/connectors.js
+++ client/src/components/Wallet/connectors.js
@@ -3,5 +3,5 @@ import { InjectedConnector } from '@web3-react/injected-connector'
 export const injected = new InjectedConnector({
     // 42 = kovan testnet
     // 1337 = ganache
-    supportedChainIds: [1, 3, 4, 5, 42, 1337],
+    supportedChainIds: [42, 1337],
 })
