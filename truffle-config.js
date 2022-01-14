0639ae90563a9add148031c291b173d4dbb5d19f
Tramy Nguyen <tramynn2@gmail.com>
2022-01-14 00:37:10 -0600

    Adds kovan configuration

--- truffle-config.js
+++ truffle-config.js
@@ -21,10 +21,9 @@ const path = require("path");
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = process.env.KOVAN_MNEMONIC;
+const infuraUrl = process.env.INFURA_URL;
 
 module.exports = {
   /**
@@ -45,9 +44,9 @@ module.exports = {
     // options below to some value.
     //
     development: {
-      host: "127.0.0.1"
-      , port: 7545
-      , network_id: "1337"
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
     },
     // Another network with more advanced options...
     // advanced: {
@@ -60,14 +59,19 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    kovan: {
+      provider: () => new HDWalletProvider(mnemonic, infuraUrl),
+      network_id: 42,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    // kovan: {
+    //   host: '127.0.0.1',
+    //   port: 7545,
+    //   network_id: '42'
+    // }
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
