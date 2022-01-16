d946ccb7b48b29fe92ea33b8d42bfc8dc22a0c5f
Tramy Nguyen <tramynn2@gmail.com>
2022-01-15 21:05:16 -0600

    Updates truffle config gas and gasPrice

--- truffle-config.js
+++ truffle-config.js
@@ -47,6 +47,11 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+      gas: 4612388,
+      gasPrice: 100000000000,
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true  
     },
     // Another network with more advanced options...
     // advanced: {
@@ -62,7 +67,8 @@ module.exports = {
     kovan: {
       provider: () => new HDWalletProvider(mnemonic, infuraUrl),
       network_id: 42,       // Ropsten's id
-      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      gas: 4612388,
+      gasPrice: 100000000000,
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
